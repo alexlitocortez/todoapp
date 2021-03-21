5142b006a4cb03d93263fd55dd663004f7733921
Alex Cortez <acim650@gmail.com>
2021-03-21 08:02:28 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,73 @@
+
+const darkImage = "/Users/l/Desktop/Frontendmentor.io/Todo App/todo-app-main/images/bg-mobile-dark.jpg";
+const lightImage = "/Users/l/Desktop/Frontendmentor.io/Todo App/todo-app-main/images/bg-mobile-light.jpg"
+const moon = "/Users/l/Desktop/Frontendmentor.io/Todo App/todo-app-main/images/icon-moon.svg";
+const sun = "/Users/l/Desktop/Frontendmentor.io/Todo App/todo-app-main/images/icon-sun.svg";
+const btn = document.getElementById('icon-image');
+
+
+const iconImage = document.getElementById('sun');
+const image = document.getElementById('background-image');
+const cols = document.getElementsByClassName('todos');
+const font = document.getElementsByClassName('font');
+const container = document.getElementsByClassName('container');
+
+
+btn.addEventListener('click', () => {
+    document.body.classList.toggle('light');
+    document.querySelector('html').classList.toggle('light');
+    if(image.src == darkImage) {
+        lightestImage();
+    } else {
+        darkestImage();
+    }
+})
+
+// Functions for line strikethough when user is done with todo item
+function strike() {
+    document.getElementById('todo-2').style.setProperty('text-decoration', 'line-through');
+    document.getElementById('containerOne').classList.toggle('checkmark-blue');
+    document.getElementById('checkmark-active-1').style.display='block';
+}
+
+function strikeOne() {
+    document.getElementById('todo-3').style.setProperty('text-decoration', 'line-through');
+    document.getElementById('containerTwo').classList.toggle('checkmark-blue');
+    document.getElementById('checkmark-active-2').style.display='block';
+}
+
+function strikeTwo() {
+    document.getElementById('todo-4').style.setProperty('text-decoration', 'line-through');
+    document.getElementById('containerThree').classList.toggle('checkmark-blue');
+    document.getElementById('checkmark-active-3').style.display='block';
+}
+
+function strikeThree() {
+    document.getElementById('todo-5').style.setProperty('text-decoration', 'line-through');
+    document.getElementById('containerFour').classList.toggle('checkmark-blue');
+    document.getElementById('checkmark-active-4').style.display='block';
+}
+
+function strikeFour() {
+    document.getElementById('todo-6').style.setProperty('text-decoration', 'line-through');
+    document.getElementById('containerFive').classList.toggle('checkmark-blue');
+    document.getElementById('checkmark-active-5').style.display='block';
+}
+
+function strikeFive() {
+    document.getElementById('todo-7').style.setProperty('text-decoration', 'line-through');
+    document.getElementById('containerSix').classList.toggle('checkmark-blue');
+    document.getElementById('checkmark-active-6').style.display='block';
+}
+
+
+
+
+
+ 
+// Can revert back to dark theme
+
+//functionalities need to code for
+// 1. drag and drop
+// 2. switch between dark/light theme
+// 3. checkmark when task completed
