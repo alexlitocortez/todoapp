e228802c0908b7862c1a5869f960b58c140c7dfe
Alex Cortez <acim650@gmail.com>
2021-04-08 10:08:28 -0700

    addnewtodo.js

--- addnewtodo.js
+++ addnewtodo.js
@@ -1,58 +1,49 @@
 
 // Add new todo
-
 function newTodo(event) {
+    // Create DOM elements
     var li = document.createElement('li');
     var inputValue = document.getElementById('todo-input').value;
     var p = document.createElement('p');
     var t = document.createTextNode(inputValue);
 
+    // paragraph that gets a text node
     p.appendChild(t);
+    // list item that appends a paragraph
     li.appendChild(p);
     li.className = 'todo-item';
     li.className += 'show';
 
     if(event.which == 13 || event.keyCode == 13 || event.onclick) {
         if(inputValue === "") {
-            alert("You must write something!");
+            alert('You must write something!');
         } else {
             document.getElementById('todo-list-id').appendChild(li);
         }
-        // document.getElementById('todo-input').value = "";
-}
-
-    var span = document.createElement('span');
-    span.className = 'close';
-    span.onclick = function() {
-        var div = this.parentElement;
-        div.style.display = 'none';
-        div.remove();
+        // makes value in input bar disappear after you press enter
+        document.getElementById('todo-input').value = "";
+        var span = document.createElement('span');
+            span.className = 'close';
+            span.onclick = function() {
+                var div = this.parentElement;
+                div.style.display = 'none';
+                div.remove();
+                countTodosRemove();
+            }
+        li.appendChild(span);
         countTodos();
-    };
-    li.appendChild(span);
-    li.onclick = function() {
-        this.classList.toggle('checked');
-        if(this.classList.contains('checked')) {
-            removeClass(this, "incomplete");
-            removeClass(this, "todo-item");
-            addClass(this, "completed");
-            countTodos();
-        } else {
-            removeClass(this, "completed");
-            addClass(this, "todo-item");
-            addClass(this, "incomplete");
-            countTodos();
         }
-    }
+}
+
+// Check number of Todo items
+function countTodos() {
+    var li = document.getElementsByClassName('close');
+    document.getElementById('todo-number').innerHTML = li.length;
 }
 
 
-
-
-
-
-
-
-
-
-
+// Check number of Todo items when closed
+function countTodosRemove() {
+    var li = document.getElementsByClassName('close');
+    document.getElementById('todo-number').innerHTML = li.length;
+}
